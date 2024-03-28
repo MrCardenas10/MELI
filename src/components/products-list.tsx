c837643d3f9bde037fdd5e070f597b517cc650e0
Estiven Cardenas <estiven@ottomotoapp.com>
2024-03-28 16:19:12 -0500

    Add TailwindCSS configuration on the project

--- src/components/products-list.tsx
+++ src/components/products-list.tsx
@@ -1,5 +1,10 @@
 import {ReactElement} from "react";
+import {Outlet} from "react-router-dom";
 
 export const ProductsList = (): ReactElement => {
-    return (<div>Product List</div>)
+    return (
+
+        <div>Product List
+    <Outlet />  
+    </div>)
 }
